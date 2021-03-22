11c1ae930e65cab27f23399afe5d9938af0eabc5
Peter Curcio <pcurcio988@gmail.com>
2021-03-21 21:11:32 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+console.log("is this working...?");
+
+(function(){
+  document.getElementById("main-title").innerHTML = "Boogie Butt";
+  console.log("this ran, too...");
+})();
+
+//console.log("second");
+
+function convertFarenheitToCelsius(temp) {
+  // get input temp
+  // apply formula
+  // return converted value
+}
+
+function convertCelsiusToFarenheit(temp) {
+
+}
