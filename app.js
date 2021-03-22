5b26e114db1de1a214397bad20a546255ac9a858
Peter Curcio <pcurcio988@gmail.com>
2021-03-22 12:26:41 -0700

    cleaning up flow/structure of app.js

--- app.js
+++ app.js
@@ -1,30 +1,24 @@
+// Debug
 let debug = false;
 
-if (debug) { console.log("is this working...?"); }
-
-// (function(){
-//   document.getElementById("main-title").innerHTML = "New Title";
-//   console.log("this ran, too...");
-// })();
-
+// Vars
 let temp = document.getElementById("temp-f").value;
-if (debug) { console.log("temp out here is: " + temp); }
+let submitButton = document.getElementById("submit-button");
+let clearButton = document.getElementById("clear-button");
 
-//let newTemp;
+// Event listeners
+submitButton.addEventListener("click", convertFarenheitToCelsius);
+submitButton.addEventListener("click", verifyClick);
+clearButton.addEventListener("click", clearInput);
 
+// Funcs
 function convertFarenheitToCelsius() {
-  // get input temp
-  // apply formula
-  // return converted value
-  // T(°C) = (T(°F) - 32) × 5/9
   let temp = document.getElementById("temp-f").value;
-  //return (temp - 32) * 5/9
-  if (debug) { console.log((temp - 32) * 5/9); }
-  //console.log("temp is: " + temp);
   newTemp = ((temp - 32) * 5/9).toPrecision(3);
-  if (debug) { console.log("newTemp is: " + newTemp); }
-  //return newTemp;
   document.getElementById("new-temp").innerHTML = newTemp + " ºC";
+
+  if (debug) { console.log((temp - 32) * 5/9); }
+  if (debug) { console.log("newTemp is: " + newTemp); }
 }
 
 function convert() {
@@ -33,28 +27,14 @@ function convert() {
   document.getElementById("new-temp").innerHTML = newTemp2;
 }
 
-let submitBtn = document.getElementById("submit-button");
-
-submitBtn.addEventListener("click", convertFarenheitToCelsius);
-submitBtn.addEventListener("click", verifyClick);
-
 function verifyClick() {
   console.log("The submit button has been clicked.");
 }
-
-document.getElementById("clear-button").addEventListener("click", clearInput);
 
 function clearInput() {
   document.getElementById("temp-f").value = "";
   document.getElementById("new-temp").innerHTML = "";
 }
-//document.getElementById("submit-button").addEventListener("click", convertFarenheitToCelsius(temp));
-
-// function testFunction() {
-//   console.log("testFunctiont test...");
-// }
-
-//document.getElementById("new-temp").innerHTML(newTemp);
 
 function convertCelsiusToFarenheit(temp) {
 
