2e4f74aca54882eacb736ed09d594382a9fa4f74
Peter Curcio <pcurcio988@gmail.com>
2021-03-21 22:16:22 -0700

    adding initial conversion functionality to console.log

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
 console.log("is this working...?");
 
-(function(){
-  document.getElementById("main-title").innerHTML = "Boogie Butt";
-  console.log("this ran, too...");
-})();
+// (function(){
+//   document.getElementById("main-title").innerHTML = "New Title";
+//   console.log("this ran, too...");
+// })();
 
-//console.log("second");
+let temp = document.getElementById("temp-f").value;
+console.log("temp out here is: " + temp);
+
+//let newTemp;
 
 function convertFarenheitToCelsius(temp) {
   // get input temp
   // apply formula
   // return converted value
+  // T(°C) = (T(°F) - 32) × 5/9
+
+  //return (temp - 32) * 5/9
+  console.log((temp - 32) * 5/9);
+  //console.log("temp is: " + temp);
+  //newTemp = (temp - 32) * 5/9);
+  //return newTemp;
 }
+
+//document.getElementById("new-temp").innerHTML(newTemp);
 
 function convertCelsiusToFarenheit(temp) {
 
